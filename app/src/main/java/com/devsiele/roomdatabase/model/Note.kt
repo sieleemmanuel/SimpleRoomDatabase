2c1fa5d65d3c8587ee1b3a3bbd207d3206716190
kimsiele <sielekim.emmanuel@gmail.com>
2022-02-04 22:38:16 +0300

    selection parcelable implementation added

--- app/src/main/java/com/devsiele/roomdatabase/model/Note.kt
+++ app/src/main/java/com/devsiele/roomdatabase/model/Note.kt
@@ -1,15 +1,19 @@
 package com.devsiele.roomdatabase.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
 
 @Entity(tableName = "notes_database_table")
+@Parcelize
 data class Note(
     @ColumnInfo(name = "note_category") var noteCategory: String="",
     @ColumnInfo(name = "note_title") var noteTitle: String="",
     @ColumnInfo(name = "note_text") var noteText: String=""
-) {
+):Parcelable{
     @PrimaryKey(autoGenerate = true)
-    var noteId: Long = 0L
+    var noteId: Long = 0
 }
